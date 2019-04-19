fix scaling... Also add specs to show_batch

audio_clip.py
@@ -1,8 +1,10 @@
 from fastai.torch_core import *
 from IPython.display import display, Audio
-import soundfile as sf
-# debug
-import sys
+import torchaudio as ta
+import torchaudio.transforms as tatfms
+import librosa
+import librosa.display
+
 
 __all__ = ['AudioClip', 'open_audio']
 
@@ -33,7 +35,7 @@ class AudioClip(ItemBase):
     def duration(self):
         return self.num_samples / self.sample_rate
 
-    def show(self, ax=None, figsize=(5, 1), player=True, title=None, **kwargs):
+    def show(self, ax=None, figsize=(5, 1), player=True, spec=True, title=None, **kwargs):
         if ax is None:
             _, ax = plt.subplots(figsize=figsize)
         if title:
@@ -45,20 +47,24 @@ class AudioClip(ItemBase):
         if player:
             # unable to display an IPython 'Audio' player in plt axes
             display(Audio(self.data, rate=self.sample_rate))
+        if spec:
+            spectfm = tatfms.Compose([
+                tatfms.MelSpectrogram(sr=self.sample_rate, n_mels=128, n_fft=2048, hop=512),
+                tatfms.SpectrogramToDB()
+                ])
+            x = self.data.unsqueeze(0)
+            spec = spectfm(x).squeeze(0).transpose(0,1)
+            nparr = spec.cpu().numpy()
+            librosa.display.specshow(nparr)
+            plt.colorbar(format='%+2.0f dB')
+            plt.title('Mel Spectrogram')
+            plt.tight_layout()
+
 
 
 def open_audio(fn):
-    # at first, use wavfile.read because it is significantly faster
-    x, sr = sf.read(fn, dtype='float32', always_2d=True)
-    x = x[:,0]
+    x, sr = ta.load(fn)
+    # Get only first channel for now
+    x = x[0,:]
 
-    t = torch.from_numpy(x.astype(np.float32, copy=False))
-    if x.dtype == np.int16:
-        t.div_(32767)
-    # not sure WHY but these show up sometimes
-    elif x.dtype == np.uint8:
-        t.sub_(127).div_(128)
-        n8 += 1
-    elif x.dtype != np.float32:
-        raise OSError('Encountered unexpected dtype: {}'.format(x.dtype))
-    return AudioClip(t, sr)
+    return AudioClip(x, sr)